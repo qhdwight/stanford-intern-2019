ca3e68e91093a8de49809691d7dfe2a9341eec12
quintin <qhdwight@gmail.com>
2019-06-19 17:55:30 -0700

    Added unique field to model, hopefully speed up checking if exsist

--- website/dashboard/management/extractor.py
+++ website/dashboard/management/extractor.py
@@ -10,7 +10,7 @@ from dashboard.models import Log
 
 LOCAL_LOGS = 's3_logs'
 
-BATCH_SIZE = 800
+BATCH_SIZE = 600
 
 
 def get_model_from_log_line(key_name, log) -> Log:
@@ -47,10 +47,10 @@ def extract_from_local_into_database():
         key_name = file_name
         log_file_number += 1
         # Skip if we have already pulled this log file
-        # if Log.objects.filter(key_name=key_name).exists():
-        #     if log_file_number % 1000 == 0:
-        #         print(f'[{datetime.now()}] Skipping through... at log #{log_file_number} with name {key_name}')
-        #     continue
+        if Log.objects.filter(key_name=key_name).exists():
+            if log_file_number % 1000 == 0:
+                print(f'[{datetime.now()}] Skipping through... at log #{log_file_number} with name {key_name}')
+            continue
         start = datetime.now()
         # Open log and create a model from each line if it has data we want
         with open(LOCAL_LOGS + '/' + file_name, 'r') as log_file:
--- website/dashboard/models.py
+++ website/dashboard/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Log(models.Model):
-    key_name=models.CharField(max_length=64, db_index=True)
+    key_name = models.CharField(max_length=64, db_index=True)
     bucket = models.CharField(max_length=64, null=True)
     time = models.DateTimeField(null=True)
     ip_address = models.GenericIPAddressField(null=True)
     requester = models.CharField(max_length=512, null=True)
-    request_id = models.CharField(max_length=1024)
-    operation = models.CharField(max_length=128, null=True)
-    s3_key = models.CharField(max_length=64, null=True)
-    request_uri = models.CharField(max_length=2048, null=True)
+    request_id = models.CharField(max_length=64, unique=True)
+    operation = models.CharField(max_length=64, null=True)
+    s3_key = models.CharField(max_length=256, null=True)
+    request_uri = models.CharField(max_length=4096, null=True)
     http_status = models.PositiveSmallIntegerField(null=True)
-    error_code = models.CharField(max_length=128, null=True)
+    error_code = models.CharField(max_length=64, null=True)
     bytes_sent = models.BigIntegerField(null=True)
     object_size = models.BigIntegerField(null=True)
     total_time = models.PositiveIntegerField(null=True)
