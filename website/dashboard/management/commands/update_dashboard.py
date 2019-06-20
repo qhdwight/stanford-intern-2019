2b0e0c3fdb3c199e3e8b3e1fa7d619d58251f9fe
quintin <qhdwight@gmail.com>
2019-06-20 16:59:07 -0700

    Update

--- website/dashboard/management/commands/update_dashboard.py
+++ website/dashboard/management/commands/update_dashboard.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
+
 import requests
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.models import Count
+from django.utils import timezone
 
-from dashboard.models import Log, MostQueried, Item, get_item_name, get_encode_url
+from dashboard.models import Log, MostQueried, Item, get_item_name, get_encode_url, IntervalQueryCount
 
 GET_JSON_HEADERS = {'accept': 'application/json'}
 
@@ -33,7 +36,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('Updating dashboard...')
-        Item.objects.all().delete()
         print('Finding most queried items via logs...')
         get_requests = Log.objects.filter(operation='REST.GET.OBJECT')
         # See which objects are queried the most
@@ -42,31 +44,34 @@ class Command(BaseCommand):
             .distinct() \
             .annotate(count=Count('s3_key')) \
             .order_by('-count')
-        limited_most_queried = most_queried_s3_keys[:6]
+        limited_most_queried = most_queried_s3_keys[:2]
         with transaction.atomic():
             for most_queried in limited_most_queried:
                 item = get_or_create_item(most_queried['s3_key'])
-                if not MostQueried.objects.filter(item=item).exists():
-                    count = most_queried['count']
-                    print(f'Creating most queried item with count {count}')
-                    MostQueried.objects.create(item=item, count=count)
+                count = most_queried['count']
+                item_query = MostQueried.objects.filter(item=item)
+                if item_query.exists():
+                    print(f'Updated queried item with name {item.s3_key} with count {count}')
+                    item_query.update(count=count)
                 else:
-                    print(f'Skipping most queried item {item}')
-        print('Done updating most queried items!')
+                    print(f'Created most queried with name {item.s3_key} with count {count}')
+                    MostQueried.objects.create(item=item, count=count)
+        print('Finished updating most queried items!')
         # See how many requests we got at different times of the year
-        # start_time = datetime(2019, 5, 1, tzinfo=timezone.get_current_timezone())
-        # end_time = timezone.now()
-        # interval = timedelta(5)
-        # time = start_time
-        # interval_query_count_models = []
-        # IntervalQueryCount.objects.all().delete()
-        # while time < end_time:
-        #     count = Log.objects.filter(time__range=(time - interval / 2, time + interval / 2)).count()
-        #     time += interval
-        #     interval_query_count_models.append(IntervalQueryCount(
-        #         time=time,
-        #         count=count
-        #     ))
-        # IntervalQueryCount.objects.bulk_create(interval_query_count_models)
-        # print(f'Created {len(interval_query_count_models)} new interval query count models')
+        start_time = datetime(2019, 5, 1, tzinfo=timezone.get_current_timezone())
+        end_time = timezone.now()
+        interval = timedelta(5)
+        time = start_time
+        with transaction.atomic():
+            while time < end_time:
+                count = Log.objects.filter(time__range=(time - interval / 2, time + interval / 2)).count()
+                time += interval
+                time_query = IntervalQueryCount.objects.filter(time=time)
+                if time_query.exists():
+                    print(f'Updated queried item with time {time} with count {count}')
+                    time_query.update(count=count)
+                else:
+                    print(f'Created most queried with time {time} with count {count}')
+                    IntervalQueryCount.objects.create(time=time, count=count)
+        print('Finished updating interval query counts')
         print('Finished updating dashboard!')
