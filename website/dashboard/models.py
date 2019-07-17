55d5b59f2810b6929803afdd2bce788d640d41c0
quintin <qhdwight@gmail.com>
2019-07-17 11:04:08 -0700

    Changed char fields to text fields and made database postgress

--- website/dashboard/models.py
+++ website/dashboard/models.py
@@ -7,9 +7,9 @@ def get_item_name(s3_key):
 
 class IpAddress(models.Model):
     ip_address = models.GenericIPAddressField(unique=True)
-    isp = models.CharField(max_length=64)
-    country = models.CharField(max_length=32)
-    city = models.CharField(max_length=32)
+    isp = models.TextField(max_length=64)
+    country = models.TextField(max_length=32)
+    city = models.TextField(max_length=32)
     latitude = models.DecimalField(decimal_places=6, max_digits=9)
     longitude = models.DecimalField(decimal_places=6, max_digits=9)
 
@@ -19,28 +19,28 @@ class QueryCountAtTime(models.Model):
     How many queries to the server were present in a interval centered around a time.
     This creates sort of a midpoint approximation of requests over time.
     """
-    data_set = models.CharField(max_length=16)
+    data_set = models.TextField(max_length=16)
     time = models.DateTimeField(db_index=True)
     count = models.PositiveIntegerField()
 
 
 class Lab(models.Model):
-    name = models.CharField(max_length=16, unique=True)
+    name = models.TextField(max_length=16, unique=True)
 
 
 class Award(models.Model):
-    name = models.CharField(max_length=16, null=True, unique=True)
-    pi = models.CharField(max_length=16, null=True)
-    project = models.CharField(max_length=8, null=True)
-    rfa = models.CharField(max_length=8, null=True)
-    status = models.CharField(max_length=8, null=True)
+    name = models.TextField(max_length=16, null=True, unique=True)
+    pi = models.TextField(max_length=16, null=True)
+    project = models.TextField(max_length=8, null=True)
+    rfa = models.TextField(max_length=8, null=True)
+    status = models.TextField(max_length=8, null=True)
 
 
 class Experiment(models.Model):
-    name = models.CharField(max_length=32, unique=True)
+    name = models.TextField(max_length=32, unique=True)
     date_released = models.DateField(null=True)
-    assay_title = models.CharField(max_length=8, null=True, db_index=True)
-    assay_term_name = models.CharField(max_length=8, null=True)
+    assay_title = models.TextField(max_length=8, null=True, db_index=True)
+    assay_term_name = models.TextField(max_length=8, null=True)
 
 
 class Item(models.Model):
@@ -49,15 +49,15 @@ class Item(models.Model):
     Information other than just the key name must be queried directly from the encode server.
     This takes time so it is done whenever necessary, not for all objects.
     """
-    s3_key = models.CharField(max_length=64, unique=True)
-    name = models.CharField(max_length=16, unique=True)
+    s3_key = models.TextField(max_length=64, unique=True)
+    name = models.TextField(max_length=16, unique=True)
     # Gathered from the encode website via REST call when needed
-    dataset = models.CharField(max_length=16, db_index=True)
-    dataset_type = models.CharField(max_length=16, db_index=True)
+    dataset = models.TextField(max_length=16, db_index=True)
+    dataset_type = models.TextField(max_length=16, db_index=True)
     experiment = models.ForeignKey(Experiment, on_delete=models.PROTECT, null=True)
     # Total amount of times it has been accessed
-    file_format = models.CharField(max_length=8, null=True)
-    file_type = models.CharField(max_length=8, null=True)
+    file_format = models.TextField(max_length=8, null=True)
+    file_type = models.TextField(max_length=8, null=True)
     award = models.ForeignKey(Award, on_delete=models.PROTECT, null=True)
     lab = models.ForeignKey(Lab, on_delete=models.PROTECT, null=True)
     date_uploaded = models.DateField(null=True)
@@ -65,25 +65,25 @@ class Item(models.Model):
 
 class Log(models.Model):
     item = models.ForeignKey(Item, null=True, on_delete=models.PROTECT)
-    key_name = models.CharField(max_length=32)
-    bucket = models.CharField(max_length=16, null=True)
+    key_name = models.TextField(max_length=32)
+    bucket = models.TextField(max_length=16, null=True)
     time = models.DateTimeField(null=True, db_index=True)
     ip_address = models.GenericIPAddressField(null=True, db_index=True)
-    requester = models.CharField(max_length=64, null=True, db_index=True)
-    request_id = models.CharField(max_length=16, unique=True)
-    operation = models.CharField(max_length=16, null=True)
-    s3_key = models.CharField(max_length=64, null=True, db_index=True)
-    request_uri = models.CharField(max_length=1024, null=True)
+    requester = models.TextField(max_length=64, null=True, db_index=True)
+    request_id = models.TextField(max_length=16, unique=True)
+    operation = models.TextField(max_length=16, null=True)
+    s3_key = models.TextField(max_length=64, null=True, db_index=True)
+    request_uri = models.TextField(max_length=1024, null=True)
     http_status = models.PositiveSmallIntegerField(null=True)
-    error_code = models.CharField(max_length=16, null=True)
+    error_code = models.TextField(max_length=16, null=True, db_index=True)
     bytes_sent = models.BigIntegerField(null=True)
     object_size = models.BigIntegerField(null=True, db_index=True)
     total_time = models.PositiveIntegerField(null=True)
     turn_around_time = models.PositiveIntegerField(null=True)
     referrer = models.URLField(null=True)
-    user_agent = models.CharField(max_length=128, null=True)
-    version_id = models.CharField(max_length=128, null=True)
-    # host_id = models.CharField(max_length=1024)
+    user_agent = models.TextField(max_length=128, null=True)
+    version_id = models.TextField(max_length=128, null=True)
+    # host_id = models.TextField(max_length=1024)
     # UNAUTHENTICATED = 0
     # SIG_V2 = 1
     # SIG_V4 = 2
@@ -94,7 +94,7 @@ class Log(models.Model):
     #         (SIG_V4, 'SigV4')
     #     ]
     # )
-    # cipher_suite = models.CharField(max_length=256)
+    # cipher_suite = models.TextField(max_length=256)
     # AUTH_HEADER = 1
     # QUERY_STRING = 2
     # auth_type = models.PositiveSmallIntegerField(
@@ -120,5 +120,5 @@ class Log(models.Model):
 
 
 class AnalysisLabItem(models.Model):
-    data_set = models.CharField(max_length=16)
-    name = models.CharField(max_length=16, unique=True)
+    data_set = models.TextField(max_length=16)
+    name = models.TextField(max_length=16, unique=True)
