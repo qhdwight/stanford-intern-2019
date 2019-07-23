afb76507ed624a22e27ed09ea2e3d9ce12d4a176
quintin <qhdwight@gmail.com>
2019-07-23 10:26:04 -0700

    AWS IP to .env

--- website/activity_viewer/settings.py
+++ website/activity_viewer/settings.py
@@ -16,7 +16,8 @@ CACHE_TIME = config('CACHE_TIME', default=3600, cast=int)
 ALLOWED_HOSTS = [
     'localhost',
     # Jupyter hub server private IP on virtual network
-    '171.67.205.36'
+    '171.67.205.36',
+    config("AWS_IP"),
 ]
 
 INSTALLED_APPS = [
