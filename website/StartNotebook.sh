4a1b0124ce72e8d286cfc7df1f9018af26cb021f
quintin <qhdwight@gmail.com>
2019-07-31 13:32:30 -0700

    Updtaed start notebook script

--- website/StartNotebook.sh
+++ website/StartNotebook.sh
@@ -1,4 +1,3 @@
 #! /bin/bash -
 
-cd ~/stanford-intern-2019/website || return
-jupyter notebook --port 8888 --ip 172.31.24.1 --no-browser
+~/anaconda3/envs/s3_usage/bin/jupyter-notebook --port 8888 --ip 172.31.24.1 --no-browser
